e9061c5b5b6f18f2ef65fdb59ef0f78b7ea64f04
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-09-23 19:16:18 +0530

    done with view directory setup

--- StudentDataBaseSystem/index.js
+++ StudentDataBaseSystem/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const ejs = require("ejs");
 const port = 8080;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/students', function(req,res){
-    res.send("Working!!")
-})
+    res.render('index');
+});
 
 app.listen(port, function(err){
     if(err){
